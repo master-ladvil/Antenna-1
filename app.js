f28d00789d5cc6aaf49e7d4b6acc1897040457fd
master-ladvil <52162632+master-ladvil@users.noreply.github.com>
2020-09-05 08:18:54 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const app = express();
 const port = process.env.PORT || 3500;
 const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(publicPath, 'index.html'));
+});
 app.listen(port, () => {
    console.log(`Server is up on port ${port}!`);
 });
